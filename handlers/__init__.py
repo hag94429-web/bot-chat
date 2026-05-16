ea6418489b5cb5e5f2137e92811729c125c79598
hag94429-web <hag94429@gmail.com>
2026-05-16 16:17:23 +0300

    Add files via upload

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,39 +1,21 @@
-from .profile import router as profile_router
-from .daily import router as daily_router
-from .bonus import router as bonus_router
-from .top import router as top_router
-
-from .shop import router as shop_router
-from .stars_shop import router as stars_shop_router
-
+from .activity import router as activity_router
 from .cases import router as cases_router
-from .roulette import router as roulette_router
-
 from .duel import router as duel_router
+from .economy import router as economy_router
 from .inventory import router as inventory_router
 from .shop_weapons import router as shop_weapons_router
-
-from .payments import router as payments_router
-
-from .activity import router as activity_router
+from .shop import router as shop_router
+from .stars import router as stars_router
+from .user import router as user_router
 
 routers = [
-    profile_router,
-    daily_router,
-    bonus_router,
-    top_router,
-
-    shop_router,
-    stars_shop_router,
-
+    activity_router,
     cases_router,
-    roulette_router,
-
     duel_router,
+    economy_router,
     inventory_router,
     shop_weapons_router,
-
-    payments_router,
-
-    activity_router
+    shop_router,
+    stars_router,
+    user_router
 ]
